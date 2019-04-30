839e079e43424fa997f3b28e8854ee96ac9c1263
Joris <jorisloemba@gmail.com>
2019-05-01 01:15:14 +0200

    Création de quelques routes pour démarrer le projet

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root  'home#index' #La page par défaut
+  get '/home', to: 'home#index' #La homepage
+  get  '/signin',    to: 'signin#index' #La page de connexion
+  get  '/signup',    to: 'signup#index' #La page d'inscription
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
